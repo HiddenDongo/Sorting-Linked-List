2007b9f830047f23d181903d4151aae682fb9eb3
Kalyan Kola Cahill <kalyankolacahill@gmail.com>
2018-02-15 11:43:51 +0000

    Finished Python Implementation

--- SortedLL.py
+++ SortedLL.py
@@ -1,6 +1,6 @@
 ##	Sorted Linked List using Sentinel Node
 ##	Kalyan Kola Cahill - C16357813
-##	DT228/2 - Software Engineering
+##	DT228/2 - 15/02/2018
 
 class SortedLL:
 
@@ -9,6 +9,7 @@ class SortedLL:
         self.dummy.next = self.dummy
         self.head = Node(self.dummy, 'Start')
 
+	#print node method
     def print_nodes(self):
         current = self.head
 
@@ -16,11 +17,13 @@ class SortedLL:
             print(current.data)
             current = current.next
 
-
+	#insert new node method
     def insert(self, value):
         new_node = Node(None, value)
         current = self.head
 
+		#iterates through Linked List until new node value <= current node
+		#value or if we've reached the end of the Linked List
         while (current.next != self.dummy and value > current.next.data):
             current = current.next
 
@@ -28,14 +31,18 @@ class SortedLL:
         current.next = new_node
 
 
+	#remove node method	
     def remove(self, value):
 
+		#checks that Linked List is not empty
         if self.isEmpty():
             print("List is Empty, you dummy!")
             return
 
         current = self.head
 
+		#iterates through Linked List until the selected value = current node 
+		#value or that we haven't reached the end of the Linked List
         while (current.next.data != value and current.next != self.dummy):
             current = current.next
 
@@ -48,7 +55,7 @@ class SortedLL:
 
 
 
-
+	#Linked List isEmpty method
     def isEmpty(self):
         current = self.head
 
@@ -57,7 +64,7 @@ class SortedLL:
         else:
             return False
 
-
+#Define Node structure
 class Node:
     def __init__(self, node, data):
         self.data = data
--- SortedLL2.py
+++ SortedLL2.py
@@ -1,8 +1,13 @@
+##	Sorted Linked List using Sentinel Node
+##	Kalyan Kola Cahill - C16357813
+##	DT228/2 - 15/02/2018
+
 class SortedLL:
 
     def __init__(self):
         self.head = Node(None, 'Start')
-
+	
+	#print node method
     def print_nodes(self):
         current = self.head
 
@@ -11,25 +16,31 @@ class SortedLL:
             current = current.next
 
 
+	#insert new node method
     def insert(self, value):
         new_node = Node(None, value)
         current = self.head
 
+		#iterates through Linked List until new node value <= current 
+		#node value
         while (current.next != None and value > current.next.data):
             current = current.next
 
         new_node.next = current.next
         current.next = new_node
 
-
+	#remove node method
     def remove(self, value):
 
+		#error message if list is empty
         if self.isEmpty():
-            print("List is Empty, you dummy!")
+            print("List is Empty")
             return
 
         current = self.head
 
+		#iterates through Linked List until current node value =
+		#selected node value
         while (current.next.data != value and current.next != None):
             current = current.next
 
@@ -38,11 +49,11 @@ class SortedLL:
             current.next = current.next.next
 
         else:
-            print("\nValue not in the list, you dummy!\n")
+            print("\nValue not in the list\n")
 
 
 
-
+	#Linked List isEmpty method
     def isEmpty(self):
         current = self.head
 
@@ -51,7 +62,7 @@ class SortedLL:
         else:
             return False
 
-
+#define Node structure
 class Node:
     def __init__(self, node, data):
         self.data = data
